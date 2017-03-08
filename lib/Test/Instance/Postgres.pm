c7a83a507b6e2557e87a459a1aeb7e817b2edb57
Tom Bloor <tom.bloor@googlemail.com>
2017-03-08 01:19:51 +0000

    First pass with static path for pg_ctl

--- lib/Test/Instance/Postgres.pm
+++ lib/Test/Instance/Postgres.pm
@@ -1,39 +1,117 @@
 package Test::Instance::Postgres;
 
-use strict;
-use 5.008_005;
-our $VERSION = '0.01';
+use Moo;
+use File::Temp;
+use File::Spec;
+use IPC::System::Simple qw/ capture /;
+use Net::EmptyPort qw/ empty_port /;
+
+use namespace::clean;
+
+our $VERSION = '0.001';
+
+has _temp_dir => (
+  is => 'lazy',
+  builder => sub {
+    return File::Temp->newdir;
+  },
+);
+
+has server_root => (
+  is => 'lazy',
+  builder => sub {
+    my $self = shift;
+    return $self->_temp_dir->dirname;
+  },
+);
+
+has db_dir => (
+  is => 'lazy',
+  builder => sub {
+    my $self = shift;
+    return File::Spec->catfile( $self->server_root, 'db' );
+  },
+);
+
+has log_file => (
+  is => 'lazy',
+  builder => sub {
+    my $self = shift;
+    return File::Spec->catfile( $self->server_root, 'pg.log' );
+  },
+);
+
+has listen_port => (
+  is => 'lazy',
+  builder => sub {
+    return empty_port;
+  },
+);
+
+has username => (
+  is => 'lazy',
+  builder => sub { 'postgres' },
+);
+
+has pg_ctl => (
+  is => 'lazy',
+  builder => sub { '/usr/lib/postgresql/9.5/bin/pg_ctl' },
+);
+
+has dsn => (
+  is => 'lazy',
+  builder => sub {
+    my $self = shift;
+    return sprintf( 'dbi:Pg:dbname=%s;host=%s;port=%s', 'postgres', 'localhost', $self->listen_port );
+  },
+);
+
+sub _init_db_cmd {
+  my $self = shift;
+
+  return join( ' ', $self->pg_ctl,
+    'initdb',
+    "-o '--auth=trust",
+    '--username=' . $self->username,
+    "' --pgdata=" . $self->db_dir,
+  );
+};
+
+sub _base_cmd {
+  my $self = shift;
+
+  return sprintf( "%s --pgdata=%s --log=%s -o '--port=%s -k %s'",
+    $self->pg_ctl,
+    $self->db_dir,
+    $self->log_file,
+    $self->listen_port,
+    $self->server_root,
+  );
+}
+
+sub run_cmd {
+  my $self = shift;
+  my $cmd = shift;
+  my $wait = shift;
+
+  $cmd = join ( ' ', '-w', $cmd ) if defined $wait;
+
+  my $full_cmd = join ( ' ', $self->_base_cmd, $cmd );
+  capture( $full_cmd );
+}
+
+sub run {
+  my $self = shift;
+
+  capture( $self->_init_db_cmd );
+  $self->run_cmd( 'start', 'wait' );
+}
+
+sub DEMOLISH {
+  my $self = shift;
+
+  #TODO Double check its dead, kill it if needed
+  $self->run_cmd( 'stop', 'wait' );
+}
 
 1;
-__END__
-
-=encoding utf-8
-
-=head1 NAME
-
-Test::Instance::Postgres - Blah blah blah
-
-=head1 SYNOPSIS
-
-  use Test::Instance::Postgres;
-
-=head1 DESCRIPTION
-
-Test::Instance::Postgres is
-
-=head1 AUTHOR
-
-Tom Bloor E<lt>t.bloor@shadowcat.co.ukE<gt>
-
-=head1 COPYRIGHT
-
-Copyright 2017- Tom Bloor
-
-=head1 LICENSE
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=head1 SEE ALSO
-
-=cut
