91b551e22df99f0e683509ba5fb3f46ad78906ca
Tom Bloor <tom.bloor@googlemail.com>
2017-03-08 00:14:47 +0000

    Initialise repo with Milla

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Test::Instance::Postgres - Blah blah blah
+
+# SYNOPSIS
+
+    use Test::Instance::Postgres;
+
+# DESCRIPTION
+
+Test::Instance::Postgres is
+
+# AUTHOR
+
+Tom Bloor <t.bloor@shadowcat.co.uk>
+
+# COPYRIGHT
+
+Copyright 2017- Tom Bloor
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Test/Instance/Postgres.pm
@@ -0,0 +1,39 @@
+package Test::Instance::Postgres;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Test::Instance::Postgres - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Test::Instance::Postgres;
+
+=head1 DESCRIPTION
+
+Test::Instance::Postgres is
+
+=head1 AUTHOR
+
+Tom Bloor E<lt>t.bloor@shadowcat.co.ukE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2017- Tom Bloor
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
